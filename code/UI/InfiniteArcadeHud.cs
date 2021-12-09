319ca87cbb7e7589c0e891e7e96e337f0a2bd9f8
BlockBuilder57 <newbrickbuilder7@gmail.com>
2021-12-09 04:18:38 -0600

    Hide HUD with devcam, clean up the SCSS a bit

--- code/UI/InfiniteArcadeHud.cs
+++ code/UI/InfiniteArcadeHud.cs
@@ -12,17 +12,20 @@ namespace infinitearcade.UI
 	{
 		public static InfiniteArcadeHud Current { get; set; }
 
+		private CrosshairCanvas m_crosshairCanvas;
+
 		public InfiniteArcadeHud()
 		{
 			if (!Host.IsClient)
 				return;
 
 			Current = this;
+			Local.Hud = this;
 
 			StyleSheet.Load("UI/InfiniteArcadeHud.scss");
 
 			// s&box base things
-			AddChild<CrosshairCanvas>();
+			m_crosshairCanvas = AddChild<CrosshairCanvas>();
 			CrosshairCanvas.SetCrosshair(new StandardCrosshair());
 			AddChild<ChatBox>();
 			AddChild<VoiceList>();
@@ -31,6 +34,17 @@ namespace infinitearcade.UI
 			// custom stuff
 			AddChild<PlayerStatus>();
 			AddChild<WeaponStatus>();
+		}
+
+		public override void Tick()
+		{
+			base.Tick();
+
+			ArcadePlayer player = Local.Pawn as ArcadePlayer;
+			if (player == null) return;
+
+			// I don't need the player right now but I'm keeping it for the future
+			m_crosshairCanvas?.SetClass("hidden", player.Camera is not FirstPersonCamera);
 		}
 
 		[Event.Hotload]
