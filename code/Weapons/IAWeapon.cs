7f24d50091119f5782a0b665fa51a7600815c954
BlockBuilder57 <newbrickbuilder7@gmail.com>
2022-02-02 23:57:58 -0600

    Move weapons to new folder pt.1, IAWeapon now inherits from BaseCarriable

--- code/Weapons/IAWeapon.cs
+++ code/Weapons/IAWeapon.cs
@@ -7,8 +7,13 @@ using System.Threading.Tasks;
 
 namespace infinitearcade
 {
-	public partial class IAWeapon : BaseWeapon, IUse
+	public partial class IAWeapon : BaseCarriable, IUse
 	{
+		public virtual float PrimaryRate => 5.0f;
+		public virtual float SecondaryRate => 15.0f;
+		[Net, Predicted] public TimeSince TimeSincePrimaryAttack { get; set; }
+		[Net, Predicted] public TimeSince TimeSinceSecondaryAttack { get; set; }
+
 		[Net, Predicted] public TimeSince TimeSinceDeployed { get; set; }
 		[Net, Predicted] public TimeSince TimeSinceDropped { get; set; }
 
@@ -17,6 +22,9 @@ namespace infinitearcade
 		public override void Spawn()
 		{
 			base.Spawn();
+
+			CollisionGroup = CollisionGroup.Weapon; // so players touch it as a trigger but not as a solid
+			SetInteractsAs(CollisionLayer.Debris); // so player movement doesn't walk into it
 
 			PickupTrigger = new PickupTrigger
 			{
@@ -34,6 +42,38 @@ namespace infinitearcade
 			base.ActiveStart(ent);
 
 			TimeSinceDeployed = 0;
+		}
+
+		public override void Simulate(Client player)
+		{
+			if (CanReload())
+				Reload();
+
+			// Reload could have changed our owner
+			if (!Owner.IsValid())
+				return;
+
+			if (CanPrimaryAttack())
+			{
+				using (LagCompensation())
+				{
+					TimeSincePrimaryAttack = 0;
+					AttackPrimary();
+				}
+			}
+
+			// AttackPrimary could have changed our owner
+			if (!Owner.IsValid())
+				return;
+
+			if (CanSecondaryAttack())
+			{
+				using (LagCompensation())
+				{
+					TimeSinceSecondaryAttack = 0;
+					AttackSecondary();
+				}
+			}
 		}
 
 		public override void OnCarryDrop(Entity dropper)
@@ -86,6 +126,70 @@ namespace infinitearcade
 			};
 
 			ViewModelEntity.SetModel(ViewModelPath);
+		}
+
+		public virtual bool CanReload()
+		{
+			if (!Owner.IsValid() || !Input.Down(InputButton.Reload)) return false;
+
+			return true;
+		}
+
+		public virtual void Reload()
+		{
+
+		}
+
+		public virtual bool CanPrimaryAttack()
+		{
+			if (!Owner.IsValid() || !Input.Down(InputButton.Attack1)) return false;
+
+			var rate = PrimaryRate;
+			if (rate <= 0) return true;
+
+			return TimeSincePrimaryAttack > (1 / rate);
+		}
+
+		public virtual void AttackPrimary()
+		{
+
+		}
+
+		public virtual bool CanSecondaryAttack()
+		{
+			if (!Owner.IsValid() || !Input.Down(InputButton.Attack2)) return false;
+
+			var rate = SecondaryRate;
+			if (rate <= 0) return true;
+
+			return TimeSinceSecondaryAttack > (1 / rate);
+		}
+
+		public virtual void AttackSecondary()
+		{
+
+		}
+
+		/// <summary>
+		/// Does a trace from start to end, does bullet impact effects. Coded as an IEnumerable so you can return multiple
+		/// hits, like if you're going through layers or ricocet'ing or something.
+		/// </summary>
+		public virtual IEnumerable<TraceResult> TraceBullet(Vector3 start, Vector3 end, float radius = 2.0f)
+		{
+			bool InWater = Physics.TestPointContents(start, CollisionLayer.Water);
+
+			var tr = Trace.Ray(start, end)
+					.UseHitboxes()
+					.HitLayer(CollisionLayer.Water, !InWater)
+					.HitLayer(CollisionLayer.Debris)
+					.Ignore(Owner)
+					.Ignore(this)
+					.Size(radius)
+					.Run();
+
+			yield return tr;
+
+			// Another trace, bullet going through thin material, penetrating water surface?
 		}
 	}
 }
