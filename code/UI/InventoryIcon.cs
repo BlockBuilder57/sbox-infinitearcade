951d6a3813ead21bf858c5dce5451527fc40909e
BlockBuilder57 <newbrickbuilder7@gmail.com>
2022-02-11 23:21:15 -0600

    Use scene panel for inventory icons
    This way I don't have to render out a bunch of images for carriables, they should just make their own things! Eventually I want to auto position the "camera" so it fits the model no matter what, but that is something for later on.

--- code/UI/InventoryIcon.cs
+++ code/UI/InventoryIcon.cs
@@ -14,6 +14,7 @@ namespace infinitearcade.UI
 		public IACarriable Carriable;
 		public Label Name;
 		public Image Icon;
+		public ScenePanel Scene;
 
 		public InventoryIcon(Panel parent, IACarriable carriable)
 		{
@@ -23,6 +24,16 @@ namespace infinitearcade.UI
 			//Icon = Add.Image("https://thiscatdoesnotexist.com/");
 
 			StyleSheet.Load("UI/InventoryIcon.scss");
+
+			using (SceneWorld.SetCurrent(new SceneWorld()))
+			{
+				Vector3 center = carriable.Model.Bounds.Center + Vector3.Left * 64;
+				SceneObject.CreateModel(carriable.WorldModelPath, Transform.Zero);
+				Light.Point(center, 512, Color.White);
+
+				Scene = Add.ScenePanel(SceneWorld.Current, center, Rotation.From(0, -90, 0), 45);
+				Scene.RenderOnce = true;
+			}
 		}
 	}
 }
